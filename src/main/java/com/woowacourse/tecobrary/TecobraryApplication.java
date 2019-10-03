752be6c55d236341796c06532072c3ee0f646c4c
thedevluffy <erased11@gmail.com>
2019-10-03 12:15:22 +0900

    feat: Application 구동시 github.yml 을 이용할 수 있도록 한다.

--- src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
+++ src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
@@ -1,13 +1,23 @@
 package com.woowacourse.tecobrary;
 
+import com.woowacourse.tecobrary.utility.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(
+        value = {
+                "classpath:github.yml"
+        },
+        ignoreResourceNotFound = true,
+        factory = YamlPropertySourceFactory.class
+)
+
 public class TecobraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TecobraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TecobraryApplication.class, args);
+    }
 
 }
