78d939b5401d9c5b958d3a2d343e705cd483b194
thedevluffy <erased11@gmail.com>
2019-10-03 16:22:45 +0900

    feat: UserService 를 구현한다.

---
+++ src/main/java/com/woowacourse/tecobrary/user/command/application/UserService.java
@@ -0,0 +1,26 @@
+package com.woowacourse.tecobrary.user.command.application;
+
+import com.woowacourse.tecobrary.user.command.domain.User;
+import com.woowacourse.tecobrary.user.command.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getByGithubId(String githubId) {
+        return userRepository.getUserByUserGithubInfo_GithubId(githubId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
--- src/main/java/com/woowacourse/tecobrary/user/command/domain/UserRepository.java
+++ src/main/java/com/woowacourse/tecobrary/user/command/domain/UserRepository.java
@@ -3,6 +3,9 @@ package com.woowacourse.tecobrary.user.command.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getUserByUserGithubInfo_GithubId(String githubId);
 }
