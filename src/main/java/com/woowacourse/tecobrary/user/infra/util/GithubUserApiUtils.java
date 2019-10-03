046bbe6fa16a4bdd8206df16682f92303e52e43c
thedevluffy <erased11@gmail.com>
2019-10-03 16:15:01 +0900

    feat: GithubUserApiUtils 에 메서드를 추가한다.

--- src/main/java/com/woowacourse/tecobrary/user/infra/util/GithubUserApiUtils.java
+++ src/main/java/com/woowacourse/tecobrary/user/infra/util/GithubUserApiUtils.java
@@ -11,14 +11,17 @@
 
 package com.woowacourse.tecobrary.user.infra.util;
 
+import com.woowacourse.tecobrary.common.util.BodyParser;
 import com.woowacourse.tecobrary.user.ui.vo.GithubEmailVo;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class GithubUserApiUtils {
@@ -31,6 +34,7 @@ public class GithubUserApiUtils {
     private static final String CODE = "code";
     private static final String SCOPE = "scope";
     private static final String API_INDEX_CONTROLLER = "/api/v1";
+    private static final String ACCESS_TOKEN = "access_token";
 
     private final String clientId;
     private final String clientSecret;
@@ -59,7 +63,23 @@ public class GithubUserApiUtils {
         }
     }
 
-    public String buildApiUrl(String code) {
+    public String getPrimaryEmail(String githubApiUserEmailClientResponse) {
+        List<GithubEmailVo> githubEmailVo = GsonUtils.parseUserEmail(githubApiUserEmailClientResponse);
+        return githubEmailVo.stream()
+                .filter(GithubEmailVo::isPrimary)
+                .map(GithubEmailVo::getEmail)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 접근"));
+    }
+
+    public String githubUserApiAccessToken(String code) {
+        String githubApiUrl = buildApiUrl(code);
+        String githubApiResponse = getGithubApiResponse(githubApiUrl);
+        Map<String, String> response = BodyParser.parse(githubApiResponse);
+        return response.get(ACCESS_TOKEN);
+    }
+
+    private String buildApiUrl(String code) {
         try {
             return new URIBuilder(GITHUB_OAUTH_ACCESS_TOKEN_BASE_URL)
                     .addParameter(CLIENT_ID, clientId)
@@ -71,12 +91,11 @@ public class GithubUserApiUtils {
         }
     }
 
-    public String getPrimaryEmail(String githubApiUserEmailClientResponse) {
-        List<GithubEmailVo> githubEmailVo = GsonUtils.parseUserEmail(githubApiUserEmailClientResponse);
-        return githubEmailVo.stream()
-                .filter(GithubEmailVo::isPrimary)
-                .map(GithubEmailVo::getEmail)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 접근"));
+    private String getGithubApiResponse(String githubApiUrl) {
+        return WebClient.create(githubApiUrl)
+                .get()
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
