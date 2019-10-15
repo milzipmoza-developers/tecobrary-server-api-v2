aca0bcea2eb54c156acce1a026f3dd9a84e8a9fd
thedevluffy <erased11@gmail.com>
2019-10-15 22:25:08 +0900

    refactor: 레거시 DB 의 자료형을 맞추기 위해 리팩토링

--- src/main/java/com/woowacourse/tecobrary/user/command/domain/UserAuthorization.java
+++ src/main/java/com/woowacourse/tecobrary/user/command/domain/UserAuthorization.java
@@ -1,17 +1,20 @@
 package com.woowacourse.tecobrary.user.command.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Embeddable
 public class UserAuthorization {
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "authorization")
     private Authorization authorization;
 
