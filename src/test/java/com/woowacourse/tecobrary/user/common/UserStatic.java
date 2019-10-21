6708ed64a1f9b649f2e59f3d86c9624e1ec20c97
thedevluffy <erased11@gmail.com>
2019-10-21 20:45:00 +0900

    test: GithubUserApiController 테스트를 구현한다.

--- src/test/java/com/woowacourse/tecobrary/user/common/UserStatic.java
+++ src/test/java/com/woowacourse/tecobrary/user/common/UserStatic.java
@@ -3,18 +3,42 @@ package com.woowacourse.tecobrary.user.common;
 import com.woowacourse.tecobrary.common.model.Email;
 import com.woowacourse.tecobrary.common.model.HttpsUrl;
 import com.woowacourse.tecobrary.user.command.domain.*;
+import com.woowacourse.tecobrary.user.ui.vo.GithubUserInfoVo;
 
 public interface UserStatic {
     String TEST_GITHUB_ID = "100";
-    UserName TEST_USER_NAME = new UserName("thedevluffy");
-    Email TEST_USER_EMAIL = new Email("thedevluffy@gmail.com");
-    HttpsUrl TEST_HTTPS_URL = new HttpsUrl("https://avatars0.githubusercontent.com/u/52121827?s=460&v=4");
+    String TEST_USER_NAME_VALUE = "thedevluffy";
+    String TEST_USER_EMAIL_VALUE = "thedevluffy@gmail.com";
+    String TEST_USER_AVATAR_URL_VALUE = "https://avatars0.githubusercontent.com/u/52121827?s=460&v=4";
+    String TEST_USER_AUTH_VALUE = "NONE";
+
+    String SAVED_GITHUB_ID = "123456";
+    String SAVED_USER_NAME_VALUE = "kwonmc";
+    String SAVED_USER_EMAIL_VALUE = "erased11@gmail.com";
+    String SAVED_USER_AVATAR_URL_VALUE = "https://temp.avatarl.url";
+    String SAVED_USER_AUTH_VALUE = "KING";
+
+    UserName TEST_USER_NAME = new UserName(TEST_USER_NAME_VALUE);
+    Email TEST_USER_EMAIL = new Email(TEST_USER_EMAIL_VALUE);
+    HttpsUrl TEST_HTTPS_URL = new HttpsUrl(TEST_USER_AVATAR_URL_VALUE);
 
     UserGithubInfo TEST_USER_GITHUB_INFO = new UserGithubInfo(
             TEST_GITHUB_ID,
             TEST_USER_NAME,
             TEST_USER_EMAIL,
             TEST_HTTPS_URL
+    );
+
+    GithubUserInfoVo TEST_GITHUB_USER_INFO_VO = new GithubUserInfoVo(
+            TEST_GITHUB_ID,
+            TEST_USER_AVATAR_URL_VALUE,
+            TEST_USER_NAME_VALUE
+    );
+
+    GithubUserInfoVo SAVED_GITHUB_USER_INFO_VO = new GithubUserInfoVo(
+            SAVED_GITHUB_ID,
+            SAVED_USER_AVATAR_URL_VALUE,
+            SAVED_USER_NAME_VALUE
     );
 
     UserAuthorization TEST_USER_AUTHORIZATION = new UserAuthorization(Authorization.USER);
---
+++ src/test/java/com/woowacourse/tecobrary/user/ui/GithubUserApiControllerTest.java
@@ -0,0 +1,81 @@
+package com.woowacourse.tecobrary.user.ui;
+
+import com.woowacourse.tecobrary.common.util.RestAssuredTestUtils;
+import com.woowacourse.tecobrary.user.command.application.UserService;
+import com.woowacourse.tecobrary.user.command.application.api.GithubApiService;
+import com.woowacourse.tecobrary.user.common.UserStatic;
+import com.woowacourse.tecobrary.user.infra.util.JwtUtils;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.Mockito.when;
+
+class GithubUserApiControllerTest extends RestAssuredTestUtils implements UserStatic {
+
+    private static final String CODE_FOR_TEST = "test_code";
+    private static final String ACCESS_TOKEN_FOR_TEST = "test_access_token";
+
+    @Mock
+    private JwtUtils jwtUtils;
+
+    @Mock
+    private GithubApiService githubApiService;
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    void initialiseRestAssuredMockMvcStandalone() {
+        RestAssuredMockMvc.standaloneSetup(new GithubUserApiController(jwtUtils, githubApiService, userService));
+    }
+
+    @DisplayName("[GET] /github/user/, data.sql 로 미리 저장하는 user 에 대한 json 이 성공적으로 반환된다.")
+    @Test
+    void getGithubUserInformationWithSavedUser() {
+        when(githubApiService.getGithubAccessToken(CODE_FOR_TEST)).thenReturn(ACCESS_TOKEN_FOR_TEST);
+        when(githubApiService.getGithubUserInfo(ACCESS_TOKEN_FOR_TEST)).thenReturn(SAVED_GITHUB_USER_INFO_VO);
+
+        given().
+                queryParam("code", CODE_FOR_TEST).
+                when().
+                get(baseUrl() + "/api/v1/tecobrary/auth").
+                then().
+                log().ifValidationFails().
+                statusCode(200).
+                contentType(JSON).
+                body("user.userNo", equalTo("1")).
+                body("user.email", equalTo(SAVED_USER_EMAIL_VALUE)).
+                body("user.name", equalTo(SAVED_USER_NAME_VALUE)).
+                body("user.avatarUrl", equalTo(SAVED_USER_AVATAR_URL_VALUE)).
+                body("user.authorization", equalTo(SAVED_USER_AUTH_VALUE));
+    }
+
+    @DisplayName("[GET] /github/user/, 새로 가입하는 user 에 대한 json 이 성공적으로 반환된다.")
+    @Test
+    void getGithubUserInformationWithNewUser() {
+        when(githubApiService.getGithubAccessToken(CODE_FOR_TEST)).thenReturn(ACCESS_TOKEN_FOR_TEST);
+        when(githubApiService.getGithubUserInfo(ACCESS_TOKEN_FOR_TEST)).thenReturn(TEST_GITHUB_USER_INFO_VO);
+        when(githubApiService.getGithubUserEmail(ACCESS_TOKEN_FOR_TEST)).thenReturn(TEST_USER_EMAIL_VALUE);
+
+        given().
+                queryParam("code", CODE_FOR_TEST).
+        when().
+                get(baseUrl() + "/api/v1/tecobrary/auth").
+        then().
+                log().ifValidationFails().
+                statusCode(200).
+                contentType(JSON).
+                body("user.userNo", equalTo("2")).
+                body("user.email", equalTo(TEST_USER_EMAIL_VALUE)).
+                body("user.name", equalTo(TEST_USER_NAME_VALUE)).
+                body("user.avatarUrl", equalTo(TEST_USER_AVATAR_URL_VALUE)).
+                body("user.authorization", equalTo(TEST_USER_AUTH_VALUE));
+    }
+}
