dd2a05f5447620819b022e6969e5021a463bfd53
skygl <jupiter9501@gmail.com>
2019-11-28 17:45:16 +0900

    [#7] refactor: ResponseEntity 를 Model 을 사용해서 메시지를 응답한다.

--- src/main/java/com/woowacourse/tecobrary/librarybook/ui/LibraryBookControllerAdvice.java
+++ src/main/java/com/woowacourse/tecobrary/librarybook/ui/LibraryBookControllerAdvice.java
@@ -4,11 +4,9 @@ import com.woowacourse.tecobrary.librarybook.exception.DuplicatedLibraryBookExce
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class LibraryBookControllerAdvice {
@@ -16,10 +14,9 @@ public class LibraryBookControllerAdvice {
     private static final Logger log = LoggerFactory.getLogger(LibraryBookControllerAdvice.class);
 
     @ExceptionHandler(DuplicatedLibraryBookException.class)
-    public ResponseEntity duplicatedLibraryBook(DuplicatedLibraryBookException e) {
+    public ResponseEntity duplicatedLibraryBook(DuplicatedLibraryBookException e, Model model) {
         log.error(e.getMessage());
-        Map<String, String> messages = new HashMap<>();
-        messages.put("message", e.getMessage());
-        return ResponseEntity.badRequest().body(messages);
+        model.addAttribute("message", e.getMessage());
+        return ResponseEntity.badRequest().body(model);
     }
 }
