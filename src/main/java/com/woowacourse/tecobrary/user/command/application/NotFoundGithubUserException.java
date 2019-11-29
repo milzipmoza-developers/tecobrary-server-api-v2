51c208672a4dcea0c209c07c0be97e8ba5e0348d
thedevluffy <erased11@gmail.com>
2019-11-29 13:08:34 +0900

    [#25] style: User 어플리케이션 레이어의 클래스들에 컨벤션을 적용한다.

--- src/main/java/com/woowacourse/tecobrary/user/command/application/NotFoundGithubUserException.java
+++ src/main/java/com/woowacourse/tecobrary/user/command/application/NotFoundGithubUserException.java
@@ -1,6 +1,18 @@
+/*
+ * @(#) NotFoundGithubUserException.java
+ *
+ * v 1.0.0
+ *
+ * 2019.11.29
+ *
+ * Copyright (c) 2019 woowacourse, thedevluffy
+ * All rights reserved
+ */
+
 package com.woowacourse.tecobrary.user.command.application;
 
 public class NotFoundGithubUserException extends RuntimeException {
+
     public NotFoundGithubUserException() {
         super("존재 하지 않는 유저");
     }
--- src/main/java/com/woowacourse/tecobrary/user/command/application/UserService.java
+++ src/main/java/com/woowacourse/tecobrary/user/command/application/UserService.java
@@ -1,3 +1,14 @@
+/*
+ * @(#) UserService.java
+ *
+ * v 1.0.0
+ *
+ * 2019.11.29
+ *
+ * Copyright (c) 2019 woowacourse, thedevluffy
+ * All rights reserved
+ */
+
 package com.woowacourse.tecobrary.user.command.application;
 
 import com.woowacourse.tecobrary.user.command.domain.*;
