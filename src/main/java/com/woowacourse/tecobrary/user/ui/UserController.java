5cad6154a5d270c307b477aee86e6414037cb73d
gch01410 <gch01410@naver.com>
2019-11-29 17:44:49 +0900

    [#6] refactor: UserController에 CrossOrigin 어노테이션을 추가한다.

--- src/main/java/com/woowacourse/tecobrary/user/ui/UserController.java
+++ src/main/java/com/woowacourse/tecobrary/user/ui/UserController.java
@@ -4,10 +4,12 @@ import com.woowacourse.tecobrary.user.command.application.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     private UserService userService;
