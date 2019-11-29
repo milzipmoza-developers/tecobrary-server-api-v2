da7eae1ea794555f11dbf8fd06a34780f43fc446
thedevluffy <erased11@gmail.com>
2019-11-29 13:09:12 +0900

    [#25] style: utility 클래스들에 컨벤션을 적용한다.

--- src/main/java/com/woowacourse/tecobrary/utility/YamlResourcePropertySource.java
+++ src/main/java/com/woowacourse/tecobrary/utility/YamlResourcePropertySource.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.support.EncodedResource;
 import java.io.IOException;
 
 public class YamlResourcePropertySource extends PropertiesPropertySource {
+
     public YamlResourcePropertySource(String name, EncodedResource resource) throws IOException {
         super(name, new YamlPropertiesProcessor(resource.getResource()).createProperties());
     }
