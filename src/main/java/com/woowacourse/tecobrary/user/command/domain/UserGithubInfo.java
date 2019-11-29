836a5567ca14d7e3e690eddf68042f6d32f4bbb8
thedevluffy <erased11@gmail.com>
2019-11-29 13:07:31 +0900

    [#25] style: User 애그리거트의 클래스들에 컨벤션을 적용한다.

--- src/main/java/com/woowacourse/tecobrary/user/command/domain/UserGithubInfo.java
+++ src/main/java/com/woowacourse/tecobrary/user/command/domain/UserGithubInfo.java
@@ -1,3 +1,14 @@
+/*
+ * @(#) UserGithubInfo.java
+ *
+ * v 1.0.0
+ *
+ * 2019.11.29
+ *
+ * Copyright (c) 2019 woowacourse, thedevluffy
+ * All rights reserved
+ */
+
 package com.woowacourse.tecobrary.user.command.domain;
 
 import com.woowacourse.tecobrary.common.model.Email;
