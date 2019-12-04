4063bd5eff2bfeaf9adbcc8d808fcd4f72042b9f
thedevluffy <thedevluffy@gmail.com>
2019-12-04 20:24:56 +0900

    [#44] feat: LibraryBookDto 를 구현한다.

---
+++ src/main/java/com/woowacourse/tecobrary/librarybook/ui/dto/LibraryBookDto.java
@@ -0,0 +1,15 @@
+package com.woowacourse.tecobrary.librarybook.ui.dto;
+
+public interface LibraryBookDto {
+    String getImage();
+
+    String getTitle();
+
+    String getAuthor();
+
+    String getPublisher();
+
+    String getIsbn();
+
+    String getDescription();
+}
