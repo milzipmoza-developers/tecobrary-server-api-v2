e86f0a705b37cf147174ee512d23f95b8168038d
이영현 <leey9494@icloud.com>
2019-12-04 15:50:25 +0900

    [#61] feat: wishBookService에 wishBook을 등록하고 id로 조회하는 메서드를 추가한다.
    - wishBook 등록 시 wishBook목록에 중복되는 isbn이 존재하면 ExistWishBookIsbnException이 발생한다.

--- src/test/java/com/woowacourse/tecobrary/wishbook/command/application/WishBookServiceTest.java
+++ src/test/java/com/woowacourse/tecobrary/wishbook/command/application/WishBookServiceTest.java
@@ -1,7 +1,10 @@
 package com.woowacourse.tecobrary.wishbook.command.application;
 
+import com.woowacourse.tecobrary.wishbook.command.domain.ExistWishBookIsbnException;
+import com.woowacourse.tecobrary.wishbook.command.domain.NotFoundWishBookException;
 import com.woowacourse.tecobrary.wishbook.command.domain.WishBook;
 import com.woowacourse.tecobrary.wishbook.command.domain.WishBookRepository;
+import com.woowacourse.tecobrary.wishbook.command.util.WishBookInfoDtoMapper;
 import com.woowacourse.tecobrary.wishbook.common.WishBookStatic;
 import com.woowacourse.tecobrary.wishbook.ui.dto.WishBookInfoDto;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +18,13 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
 class WishBookServiceTest implements WishBookStatic {
@@ -32,12 +38,54 @@ class WishBookServiceTest implements WishBookStatic {
     @DisplayName("특정 페이지의 wishBook list를 조회한다.")
     @Test
     void successfullyFindWishBooksOnPage() {
-        List<WishBook> mockWishBooks = Arrays.asList(TEST_WISHBOOK, TEST_WISHBOOK_01);
+        List<WishBook> mockWishBooks = Arrays.asList(TEST_WISH_BOOK, TEST_WISH_BOOK_01);
 
         given(wishBookRepository.findAll(any(PageRequest.class)))
                 .willReturn(new PageImpl<>(mockWishBooks, PageRequest.of(1, 2), 2));
 
         List<WishBookInfoDto> wishBooks = wishBookService.findWishBooksOnPage(1, 2);
+
         assertThat(wishBooks).hasSize(2);
+    }
+
+    @DisplayName("wishBook에 책 정보를 등록한다.")
+    @Test
+    void successfullySaveWishBook() {
+        given(wishBookRepository.existsByWishBookInfoIsbn(TEST_CREATE_ISBN)).willReturn(false);
+        given(wishBookRepository.save(TEST_CREATE_WISH_BOOK)).willReturn(TEST_CREATE_WISH_BOOK);
+
+        wishBookService.createWishBook(WishBookInfoDtoMapper.toDto(TEST_CREATE_WISH_BOOK));
+
+        verify(wishBookRepository).save(TEST_CREATE_WISH_BOOK);
+    }
+
+    @DisplayName("해당하는 Isbn이 존재하면 등록에 실패한다.")
+    @Test
+    void failedSaveWishBook() {
+        given(wishBookRepository.existsByWishBookInfoIsbn(TEST_CREATE_ISBN)).willReturn(true);
+        given(wishBookRepository.save(TEST_CREATE_WISH_BOOK)).willReturn(TEST_CREATE_WISH_BOOK);
+
+        assertThrows(ExistWishBookIsbnException.class
+                , () -> wishBookService.createWishBook(WishBookInfoDtoMapper.toDto(TEST_CREATE_WISH_BOOK)));
+    }
+
+    @DisplayName("해당하는 id 로 WishBook 을 조회한다.")
+    @Test
+    void successfullyFindWishBook() {
+        given(wishBookRepository.findById(1L)).willReturn(Optional.of(TEST_WISH_BOOK));
+
+        WishBookInfoDto wishBookInfoDto = wishBookService.findById(1L);
+
+        assertThat(wishBookInfoDto.getIsbn()).isEqualTo(TEST_ISBN);
+        assertThat(wishBookInfoDto.getUserId()).isEqualTo(TEST_USER_ID);
+    }
+
+    @DisplayName("해당하는 id가 없을 때 조회에 실패한다.")
+    @Test
+    void failedFindWishBook() {
+        given(wishBookRepository.findById(1L)).willReturn(Optional.empty());
+
+        assertThrows(NotFoundWishBookException.class
+                , () -> wishBookService.findById(1L));
     }
 }
