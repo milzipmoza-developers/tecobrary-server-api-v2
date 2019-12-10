77c3bc66947fb1f4be244cc01bab1bdda2cca8b6
thedevluffy <thedevluffy@gmail.com>
2019-12-10 14:41:18 +0900

    [#59] feat: SerialRepository 메서드에 추가한다.
    - findBySerialInfoSerialNumber : SerialNumber 로 Serial 을 찾는 메서드
    - existsBySerialInfoSerialNumberAndSerialRentStatusIsTrue : Serial 이 존재하고 대여중이 아닌 Serial 을 찾는 메서드

--- src/main/java/com/woowacourse/tecobrary/serial/domain/SerialRepository.java
+++ src/main/java/com/woowacourse/tecobrary/serial/domain/SerialRepository.java
@@ -4,13 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SerialRepository extends JpaRepository<Serial, Long> {
+
+    Optional<Serial> findBySerialInfoSerialNumber(Long serialNumber);
 
     boolean existsBySerialInfoSerialNumber(Long serialNumber);
 
     List<Serial> findAllBySerialLibraryBookBookId(Long bookId);
 
     void deleteBySerialInfoSerialNumber(Long serialNumber);
+
+    boolean existsBySerialInfoSerialNumberAndSerialRentStatusIsTrue(Long id);
 }
