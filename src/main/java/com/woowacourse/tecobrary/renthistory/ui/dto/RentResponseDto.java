1cb062a4f7a8c653acadae247508d8bf16cab4b1
thedevluffy <thedevluffy@gmail.com>
2019-12-11 16:58:57 +0900

    [#60] refactor: RentHistoryResponseDto 를 상속하도록 구현한다.

--- src/main/java/com/woowacourse/tecobrary/renthistory/ui/dto/RentResponseDto.java
+++ src/main/java/com/woowacourse/tecobrary/renthistory/ui/dto/RentResponseDto.java
@@ -1,3 +1,14 @@
+/*
+ * @(#) RentResponseDto.java
+ *
+ * v 1.0.0
+ *
+ * 2019.12.10
+ *
+ * Copyright (c) 2019 woowacourse, thedevluffy
+ * All rights reserved
+ */
+
 package com.woowacourse.tecobrary.renthistory.ui.dto;
 
 import lombok.Getter;
@@ -5,7 +16,10 @@ import lombok.Getter;
 @Getter
 public class RentResponseDto extends RentHistoryResponseDto {
 
+    private RentInfoDto rentInfo;
+
     public RentResponseDto(final RentInfoDto rentInfo, final String message) {
-        super(rentInfo, message);
+        super(message);
+        this.rentInfo = rentInfo;
     }
 }
