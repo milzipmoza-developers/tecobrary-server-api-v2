c6bd1af059a81e9b0046d80e6776445d420aa10f
thedevluffy <thedevluffy@gmail.com>
2019-12-11 17:01:34 +0900

    [#60] feat: RentReturnController 에 returnBook 기능을 추가한다.

--- src/main/java/com/woowacourse/tecobrary/renthistory/ui/RentReturnController.java
+++ src/main/java/com/woowacourse/tecobrary/renthistory/ui/RentReturnController.java
@@ -1,21 +1,31 @@
+/*
+ * @(#) RentReturnController.java
+ *
+ * v 1.0.0
+ *
+ * 2019.12.10
+ *
+ * Copyright (c) 2019 woowacourse, milzipmoza-developers, thedevluffy
+ * All rights reserved
+ */
+
 package com.woowacourse.tecobrary.renthistory.ui;
 
 import com.woowacourse.tecobrary.renthistory.application.RentReturnService;
 import com.woowacourse.tecobrary.renthistory.ui.dto.RentRequestDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.woowacourse.tecobrary.renthistory.ui.dto.RentResponseDto;
+import com.woowacourse.tecobrary.renthistory.ui.dto.ReturnRequestDto;
+import com.woowacourse.tecobrary.renthistory.ui.dto.ReturnResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 public class RentReturnController {
 
-    private static final Logger log = LoggerFactory.getLogger(RentReturnController.class);
+    private static final String RENT_SUCCESS_MESSAGE = "대여에 성공하였습니다.";
+    private static final String RETURN_SUCCESS_MESSAGE = "반납에 성공하였습니다.";
 
     private final RentReturnService rentReturnService;
 
@@ -26,7 +36,17 @@ public class RentReturnController {
 
     @PostMapping("/rents")
     public ResponseEntity rent(@RequestBody final RentRequestDto rentRequestDto) {
-        log.debug("rentRequestDto : {}", rentRequestDto);
-        return ResponseEntity.ok(rentReturnService.rent(rentRequestDto));
+        return ResponseEntity.ok(new RentResponseDto(
+                rentReturnService.rent(rentRequestDto),
+                RENT_SUCCESS_MESSAGE)
+        );
+    }
+
+    @PatchMapping("/rents")
+    public ResponseEntity returnBook(@RequestBody final ReturnRequestDto returnRequestDto) {
+        return ResponseEntity.ok(new ReturnResponseDto(
+                rentReturnService.returnBook(returnRequestDto),
+                RETURN_SUCCESS_MESSAGE)
+        );
     }
 }
