6fcb2ab4ca85890fa77207cca9d7c3d8dcd904fe
thedevluffy <thedevluffy@gmail.com>
2019-12-12 14:44:16 +0900

    [#43] chore: spring cloud 로 받아올 수 있도록 설정을 변경한다.

--- src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
+++ src/main/java/com/woowacourse/tecobrary/TecobraryApplication.java
@@ -1,24 +1,14 @@
 package com.woowacourse.tecobrary;
 
-import com.woowacourse.tecobrary.utility.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-@PropertySource(
-        value = {
-                "classpath:github.yml",
-                "classpath:jwtauth.yml"
-        },
-        ignoreResourceNotFound = true,
-        factory = YamlPropertySourceFactory.class
-)
-
 public class TecobraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TecobraryApplication.class, args);
     }
-
 }
