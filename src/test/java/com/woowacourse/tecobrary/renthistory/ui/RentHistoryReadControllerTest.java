f32394f6e820d8a624cbba676a0222ec989efc38
thedevluffy <thedevluffy@gmail.com>
2019-12-25 21:38:08 +0900

    [#133] feat: 유저의 반납 내역을 조회하는 기능에 대한 인수테스트를 추가한다.

--- src/test/java/com/woowacourse/tecobrary/renthistory/ui/RentHistoryReadControllerTest.java
+++ src/test/java/com/woowacourse/tecobrary/renthistory/ui/RentHistoryReadControllerTest.java
@@ -52,4 +52,35 @@ class RentHistoryReadControllerTest extends AcceptanceTestUtils implements RentH
                 body("[1].userId", equalTo(TEST_RENT_HISTORY_USER_ID_16.intValue())).
                 body("[1].rentDate", equalTo("2019-11-11T03:48:41"));
     }
+
+    @DisplayName("[GET] /histories/:userId, 회원의 도서반납 목록을 조회한다.")
+    @Test
+    void successfullyFindAllReturnHistoryByUser() {
+
+        given(this.spec).
+                accept(JSON).
+                filter(document(DOCUMENTATION_OUTPUT_DIRECTORY,
+                        pathParameters(
+                                parameterWithName("user_id").description("반납 목록을 조회할 유저의 id")),
+                        responseFields(
+                                fieldWithPath("[0].id").description("RentHistory 테이블의 id"),
+                                fieldWithPath("[0].serial").description("해당 도서의 Serial Number"),
+                                fieldWithPath("[0].title").description("해당 도서의 제목"),
+                                fieldWithPath("[0].userId").description("해당 도서를 대여한 유저 id"),
+                                fieldWithPath("[0].rentDate").description("해당 도서를 대여한 날짜 및 시간"),
+                                fieldWithPath("[0].returnDate").description("해당 도서를 반납한 날짜 및 시간")))).
+        when().
+                get(baseUrl("/histories/{user_id}"), 33).
+        then().
+                log().ifError().
+                log().ifValidationFails().
+                statusCode(200).
+                contentType(JSON).
+                body("[0].id", equalTo(45)).
+                body("[0].serial", equalTo(10)).
+                body("[0].title", equalTo("객체 지향과 디자인 패턴 (개발자가 반드시 정복해야 할)")).
+                body("[0].userId", equalTo(33)).
+                body("[0].rentDate", equalTo("2019-11-15T03:08:39")).
+                body("[0].returnDate", equalTo("2019-11-15T04:05:49"));
+    }
 }
