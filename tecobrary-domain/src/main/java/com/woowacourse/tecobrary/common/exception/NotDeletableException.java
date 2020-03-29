73634e69558c8cbaeead3936bc01a1bced0a756a
thedevluffy <thedevluffy@gmail.com>
2020-03-29 17:30:53 +0900

    [#155] feat: 공통 클래스를 구현한다.

---
+++ tecobrary-domain/src/main/java/com/woowacourse/tecobrary/common/exception/NotDeletableException.java
@@ -0,0 +1,10 @@
+package com.woowacourse.tecobrary.common.exception;
+
+public class NotDeletableException extends RuntimeException {
+
+    private static final String NOT_DELETABLE_EXCEPTION_MESSAGE = "이미 삭제된 데이터는 삭제할 수 없습니다.";
+
+    public NotDeletableException() {
+        super(NOT_DELETABLE_EXCEPTION_MESSAGE);
+    }
+}
