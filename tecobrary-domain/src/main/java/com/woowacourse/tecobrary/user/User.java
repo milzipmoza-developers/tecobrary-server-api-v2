d59212865eaceb2bae9ed88515058c81f2c590da
thedevluffy <thedevluffy@gmail.com>
2020-03-29 17:31:07 +0900

    [#155] feat: User 를 구현한다.

--- tecobrary-domain/src/main/java/com/woowacourse/tecobrary/user/User.java
+++ tecobrary-domain/src/main/java/com/woowacourse/tecobrary/user/User.java
@@ -1,4 +1,56 @@
 package com.woowacourse.tecobrary.user;
 
-public class User {
+import com.woowacourse.tecobrary.common.entity.ModifiableEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class User extends ModifiableEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 25)
+    private String githubId;
+
+    @Column(nullable = false, length = 25)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String avatarUrl;
+
+    @Column(nullable = false)
+    private String authorization;
+
+    @Builder
+    public User(String githubId,
+                String name,
+                String email,
+                String avatarUrl,
+                String authorization) {
+        this.githubId = githubId;
+        this.name = name;
+        this.email = email;
+        this.avatarUrl = avatarUrl;
+        this.authorization = authorization;
+    }
+
+    public String updateName(String name) {
+        if (name.length() > 25) {
+            throw new IllegalArgumentException("이름은 25자를 초과할 수 없습니다.");
+        }
+        return this.name = name;
+    }
+
+    public String updateAuthorization(String authorization) {
+        return this.authorization = authorization;
+    }
 }
