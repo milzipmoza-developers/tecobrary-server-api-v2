6ffe49bc5b644bca79364d2b5f4abc731b6d42ef
thedevluffy <thedevluffy@gmail.com>
2020-04-13 21:46:42 +0900

    [#156] feat: WishBook 에 필드를 추가한다

--- tecobrary-domain/src/main/java/com/woowacourse/tecobrary/domain/wishbook/entity/WishBook.java
+++ tecobrary-domain/src/main/java/com/woowacourse/tecobrary/domain/wishbook/entity/WishBook.java
@@ -36,6 +36,13 @@ public class WishBook extends DeletableEntity {
             columnDefinition = "varchar(500) default '내용 없음'")
     private String description;
 
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private WishBookStatus status;
+
+    @Column(nullable = false, length = 100)
+    private String reason;
+
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "wish_book_request_user_id")
     private User user;
@@ -47,6 +54,7 @@ public class WishBook extends DeletableEntity {
                     String publisher,
                     String isbn,
                     String description,
+                    String reason,
                     User user) {
         this.image = image;
         this.title = title;
@@ -54,7 +62,21 @@ public class WishBook extends DeletableEntity {
         this.publisher = publisher;
         this.isbn = isbn;
         this.description = description;
+        this.status = WishBookStatus.REQUESTED;
+        this.reason = reason;
         this.user = user;
+    }
+
+    public void enrollWishBook(String reason) {
+        this.status = WishBookStatus.ENROLLED;
+        this.reason = reason;
+        this.softDelete();
+    }
+
+    public void cancelWishBook(String reason) {
+        this.status = WishBookStatus.CANCELED;
+        this.reason = reason;
+        this.softDelete();
     }
 
     @Override
---
+++ tecobrary-domain/src/main/java/com/woowacourse/tecobrary/domain/wishbook/entity/WishBookStatus.java
@@ -0,0 +1,8 @@
+package com.woowacourse.tecobrary.domain.wishbook.entity;
+
+public enum WishBookStatus {
+    REQUESTED, // 요청등록
+    ENROLLED, // LibraryBook 등록
+    CANCELED, // 취소
+    ;
+}
