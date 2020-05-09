e638258bd9026f65c0a5ea3882c57ae1b4d2d37e
thedevluffy <thedevluffy@gmail.com>
2020-05-09 20:51:21 +0900

    [#155] edit: 새로운 DefaultControllerAdvice 를 구현한다

--- tecobrary-core-api/src/main/java/com/woowacourse/tecobrary/common/DefaultControllerAdvice.java
+++ tecobrary-core-api/src/main/java/com/woowacourse/tecobrary/common/DefaultControllerAdvice.java
@@ -1,16 +1,14 @@
-package com.woowacourse.tecobrary.common.ui;
+package com.woowacourse.tecobrary.common;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class CommonControllerAdvice {
-
-    private static final Logger log = LoggerFactory.getLogger(CommonControllerAdvice.class);
+@Slf4j
+public class DefaultControllerAdvice {
 
     @ExceptionHandler({NumberFormatException.class,})
     public ResponseEntity handleException(final RuntimeException e, final Model model) {
