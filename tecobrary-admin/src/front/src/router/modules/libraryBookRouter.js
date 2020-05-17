83e02eb67dfbfdfba5f356fca12ecfbecfc6f2b3
thedevluffy <thedevluffy@gmail.com>
2020-05-17 20:10:23 +0900

    [#156] feat: 신규 장서 등록 기능을 구현한다

--- tecobrary-admin/src/front/src/router/modules/libraryBookRouter.js
+++ tecobrary-admin/src/front/src/router/modules/libraryBookRouter.js
@@ -1,30 +1,34 @@
 import Layout from '@/layout'
 
 const libraryBookRouter = {
-    path: '/libraryBook',
-    component: Layout,
-    redirect: '/libraryBook/list',
-    name: '장서 관리',
-    meta: {
-        title: '장서 관리',
-        icon: 'education'
+  path: '/libraryBook',
+  component: Layout,
+  redirect: '/libraryBook/list',
+  name: '장서 관리',
+  meta: {
+    title: '장서 관리',
+    icon: 'education',
+    roles: ['ROLE_ADMIN']
+  },
+  children: [
+    {
+      path: 'index',
+      component: () => import('@/views/librarybook/index'),
+      name: '보유 장서 관리',
+      meta: {
+        title: '보유 장서 관리',
+        roles: ['ROLE_ADMIN']
+      },
     },
-    children: [
-        {
-            path: 'index',
-            component: () => import('@/views/librarybook/index'),
-            name: '보유 장서 관리',
-            meta: { title: '보유 장서 관리' },
-            roles: ['ROLE_ADMIN']
-        },
-        {
-            path: 'new',
-            component: () => import('@/views/librarybook/new'),
-            name: '신규 장서 등록',
-            meta: { title: '신규 장서 등록' },
-            roles: ['ADMIN']
-        }
-    ]
+    {
+      path: 'new',
+      component: () => import('@/views/librarybook/new'),
+      name: '신규 장서 등록',
+      meta: {title: '신규 장서 등록',
+        roles: ['ROLE_ADMIN']
+      },
+    }
+  ]
 }
 
 export default libraryBookRouter
--- tecobrary-admin/src/front/src/router/modules/rentHistoryRouter.js
+++ tecobrary-admin/src/front/src/router/modules/rentHistoryRouter.js
@@ -1,37 +1,44 @@
 import Layout from '@/layout'
 
 const userRouter = {
-    path: '/history',
-    component: Layout,
-    redirect: '/history/index',
-    name: '대여내역',
-    meta: {
-        title: '대여내역',
-        icon: 'documentation'
+  path: '/history',
+  component: Layout,
+  redirect: '/history/index',
+  name: '대여내역',
+  meta: {
+    title: '대여내역',
+    icon: 'documentation',
+    roles: ['ROLE_ADMIN']
+  },
+  children: [
+    {
+      path: 'index',
+      component: () => import('@/views/renthistory/index'),
+      name: '대시보드',
+      meta: {
+        title: '대시보드',
+        roles: ['ROLE_ADMIN']
+      },
     },
-    children: [
-        {
-            path: 'index',
-            component: () => import('@/views/renthistory/index'),
-            name: '대시보드',
-            meta: { title: '대시보드' },
-            roles: ['ROLE_ADMIN']
-        },
-        {
-            path: 'rent',
-            component: () => import('@/views/renthistory/rent'),
-            name: '대여 현황',
-            meta: { title: '대여 현황' },
-            roles: ['ROLE_ADMIN']
-        },
-        {
-            path: 'return',
-            component: () => import('@/views/renthistory/return'),
-            name: '반납 기록',
-            meta: { title: '반납 기록' },
-            roles: ['ROLE_ADMIN']
-        }
-    ]
+    {
+      path: 'rent',
+      component: () => import('@/views/renthistory/rent'),
+      name: '대여 현황',
+      meta: {
+        title: '대여 현황',
+        roles: ['ROLE_ADMIN']
+      },
+    },
+    {
+      path: 'return',
+      component: () => import('@/views/renthistory/return'),
+      name: '반납 기록',
+      meta: {
+        title: '반납 기록',
+        roles: ['ROLE_ADMIN']
+      },
+    }
+  ]
 }
 
 export default userRouter
